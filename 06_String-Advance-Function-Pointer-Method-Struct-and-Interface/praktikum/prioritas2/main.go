d04000ce319cc1b699d60a2af8e3749ac30cb182
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-24 14:48:25 +0700

    fix: done

--- 06_String-Advance-Function-Pointer-Method-Struct-and-Interface/praktikum/prioritas2/main.go
+++ 06_String-Advance-Function-Pointer-Method-Struct-and-Interface/praktikum/prioritas2/main.go
@@ -21,42 +21,15 @@ func caesar(offset int, input string) string {
 
 func main() {
 
-	// fmt.Println(caesar(3, "abc")) // def
+	fmt.Println(caesar(3, "abc")) // def
 
-	// fmt.Println(caesar(2, "alta")) // def
+	fmt.Println(caesar(2, "alta")) // def
 
-	// fmt.Println(caesar(10, "alterraacademy")) // kvdobbkkmknowi
+	fmt.Println(caesar(10, "alterraacademy")) // kvdobbkkmknowi
 
-	// fmt.Println(caesar(1, "abcdefghijklmnopqrstuvwxyz")) // bcdefghijklmnopqrstuvwxyza
+	fmt.Println(caesar(1, "abcdefghijklmnopqrstuvwxyz")) // bcdefghijklmnopqrstuvwxyza
 
 	fmt.Println(caesar(1000, "abcdefghijklmnopqrstuvwxyz")) // mnopqrstuvwxyzabcdefghijkl
-
-	resultASCII := 1 + 1000
-
-	for resultASCII > 26 {
-		fmt.Print(resultASCII, " ")
-		resultASCII = 1 + (resultASCII % 26)
-	}
-
-	
-
-	// for resultASCII > 26 {
-	// 	fmt.Print(resultASCII, " ")
-	// 	resultASCII = 96 + (resultASCII - 26)
-	// }
-
-	// fmt.Println(resultASCII)
-
-	// fmt.Println("============")
-
-
-	// resultASCII = 2 + 27
-	// for resultASCII > 26 {
-	// 	fmt.Print(resultASCII, " ")
-	// 	resultASCII = resultASCII - 26
-	// }
-
-	// fmt.Println(resultASCII)
 
 
 }
