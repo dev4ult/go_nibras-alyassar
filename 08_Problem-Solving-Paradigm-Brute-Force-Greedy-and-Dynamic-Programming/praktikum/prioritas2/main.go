076372c9ac3b920ae49d7df97aacfb8b6276ec62
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-26 15:21:58 +0700

    fix: not done

--- 08_Problem-Solving-Paradigm-Brute-Force-Greedy-and-Dynamic-Programming/praktikum/prioritas2/main.go
+++ 08_Problem-Solving-Paradigm-Brute-Force-Greedy-and-Dynamic-Programming/praktikum/prioritas2/main.go
@@ -1,11 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
 
 func Frog(jumps []int) int {
 
+	var totalOptimum []int
 
+	sort.Ints(totalOptimum)
+	return totalOptimum[len(totalOptimum) - 1]
+
+}
+
+
+// Nyobain metode greedy (tidak selalu optimum)
+func GreedyFrog(jumps []int) int {
+	var totalOptimum int = 0
+
+	for i := 0; i < len(jumps) - 1; i++ {		
+		oneStep := math.Abs(float64(jumps[i + 1]) - float64(jumps[i]))
+		twoSteps := math.Abs(float64(jumps[i + 2]) - float64(jumps[i]))
+		
+		if twoSteps > oneStep {
+			totalOptimum += int(twoSteps)
+			i += 2
+			continue
+		}
+
+		if oneStep > twoSteps {
+			totalOptimum += int(oneStep)
+			i++
+			continue
+		}
+	}
+
+	return totalOptimum
 }
 
 
