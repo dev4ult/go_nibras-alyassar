355493f2c81880f9d61a7831c4a534cb50128877
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-26 15:21:50 +0700

    fix: exploration practice done

--- 08_Problem-Solving-Paradigm-Brute-Force-Greedy-and-Dynamic-Programming/praktikum/eksplorasi/main.go
+++ 08_Problem-Solving-Paradigm-Brute-Force-Greedy-and-Dynamic-Programming/praktikum/eksplorasi/main.go
@@ -2,29 +2,42 @@ package main
 
 import "fmt"
 
+type conversion struct {
+	value int
+	digit string
+}
+
 func RomanNumerals(number int) string {
+		
+	conversions := []conversion{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
 
 	var romanNumber string = ""
 
-
-	romanTen := number / 10 
-	if romanTen != 0 {
-		for i := 0; i < romanTen; i++ {
-			romanNumber += "X"
+	for _, obj := range conversions {
+		for obj.value <= number {
+			number -= obj.value
+			romanNumber += obj.digit
 		}
 	}
-
-	romanFive := number % 10 / 5
-	if romanFive != 0 {
-		for i := 0; i < romanFive; i++ {
-			romanNumber += "V"
-		}
-	}
-
+	
 	return romanNumber
 }
 
 func main() {
 
-	fmt.Println(RomanNumerals(25))
+	fmt.Println(RomanNumerals(1646))
 }
