d8798ee9f39b52ccfa84110f0e8ae3d4a4779e22
Nibras Alyassar <98545017+dev4ult@users.noreply.github.com>
2023-09-01 15:50:23 +0700

    Update main.go

--- 08_Problem-Solving-Paradigm-Brute-Force-Greedy-and-Dynamic-Programming/praktikum/prioritas1/soal4/main.go
+++ 08_Problem-Solving-Paradigm-Brute-Force-Greedy-and-Dynamic-Programming/praktikum/prioritas1/soal4/main.go
@@ -1,15 +1,23 @@
 package main
 
-func SimpleEquations(A, B, C int) {
-
-	var x, y, z int
-
+func EquationXYZ(a, b, c int) {
+	for x := 1; x <= 1000; x++ {
+		for y := 1; y <= 1000; y++ {
+			for z := 1; z <= 1000; z++ {
+				if x+y+z == a && x*y*z == b {
+					fmt.Println(x, y, z)
+					return
+				}
+			}
+		}
+	}
+	fmt.Println("no solution")
 }
 
 func main() {
 
-	SimpleEquations(1, 2, 3) // no solution
+	EquationXYZ(1, 2, 3) // no solution
 
-	SimpleEquations(6, 6, 14) // 1 2 3
+	EquationXYZ(6, 6, 14) // 1 2 3
 
 }
