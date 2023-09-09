4d61ec94d1abc098a11dcff533b5f1ebc251142a
dev4ult <nibrasbiasaaja@gmail.com>
2023-09-10 06:41:38 +0700

    fix: more practice answer

--- 12_Join-Union-Agregation-Subquery-Function-(DBMS)/praktikum/soal2/joinUnionAgregate.sql
+++ 12_Join-Union-Agregation-Subquery-Function-(DBMS)/praktikum/soal2/joinUnionAgregate.sql
@@ -17,3 +17,27 @@ ON transaction_details.transaction_id = transactions.id
 JOIN products
 ON transaction_details.product_id = products.id
 WHERE products.product_type_id = 2;
+
+SELECT 
+  products.*,  product_types.name as 'product type'
+FROM products
+JOIN product_types
+ON products.product_type_id = product_types.id;
+
+SELECT
+  users.name as 'user name', products.name as 'product name', transaction_details.*
+FROM transaction_details
+JOIN products
+ON products.id = transaction_details.product_id
+JOIN transactions
+ON transactions.id = transaction_details.transaction_id
+JOIN users
+ON users.id = transactions.user_id;
+
+DELIMITER //
+CREATE FUNCTION DeleteDetailsAfterTransaction(id INT)
+RETURNS INT
+
+
+
+
