bda9b6c5e559d5789cb39e02a68770b1282ffc2f
dev4ult <nibrasbiasaaja@gmail.com>
2023-10-01 22:03:40 +0700

    fix: minor change

--- 19_Unit-Testing/praktikum/restapi-integrationtest/controllers/book-controller_test.go
+++ 19_Unit-Testing/praktikum/restapi-integrationtest/controllers/book-controller_test.go
@@ -1,1 +1,40 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+
+	model "praktikum/models"
+)
+
+var (
+	db = &gorm.DB{}
+	MockBookModel = model.NewBookModel(db)
+	MockBookController = NewBookController(MockBookModel)
+	
+	BookExampleData = model.Book{Title: "Book 1", Author: "Nibras", Publisher: "PT. Gremadia"}
+)
+
+func TestCreateBook(t *testing.T) {
+	var e = echo.New()
+
+	var req = httptest.NewRequest(http.MethodPost, "/", nil)
+	var res = httptest.NewRecorder()
+
+	var c = e.NewContext(req, res)
+	c.SetPath("/books")
+
+	var tmp = map[string]any{}
+
+	var response = res.Body.Bytes()
+	err := json.Unmarshal(response, &tmp)
+
+	assert.Equal(t, 200, res.Code)
+	assert.Error(t, err)
+}
---
+++ 20_Clean-and-Hexagonal-Architecture/README.md
@@ -0,0 +1,7 @@
+# Clean and Hexagonal Architecture
+
+### Poin poin pembelajaran
+
+1. **Clean Architecture** adalah pola arsitektur *software* yang bertujuan untuk menciptakan struktur yang modular, mudah *maintenance*, dan mudah untuk di *testing*.
+2. **Kelebihan** dari *Clean Architecture* adalah fleksibilitas dalam *development* dan perubahan tanpa merusak *main business logic*.
+3. **Domain Driven Design** merupakan pendekatan dalam *software development* yang berfokus pada pemahaman dan perwakilan yang kuat dari *business domain* dalam *software* yang dibuat.
